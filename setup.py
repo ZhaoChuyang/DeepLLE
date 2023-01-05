aa7d551509ff0c261d471a3d7adfa07b25fdafb7
ZhaoChuyang <zcy201420752@gmail.com>
2023-01-05 20:56:34 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DeepLLE',
-    version='0.0.1',
+    name='deeplle',
+    version='1.0.0',
+    author="Intel",
     description='Distributed PyTorch Training Framework for Low-light Enhancement',
-    packages=find_packages(),
+    packages=find_packages(exclude=("configs", "tests*")),
+    python_requires='>=3.7',
     install_requires=[
-        'torch',
-        'numpy',
-        'tqdm',
-    ],
+        "torch>=1.7.0",
+        "torchvision>=0.8.0",
+        "pandas",
+        "tensorboard",
+        "tqdm",
+        "termcolor",
+    ]
 )
